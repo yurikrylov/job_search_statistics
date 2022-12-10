3112ec3aa1daed123578f3e1c018f7952935fd59
Yuri Krylov <yuri.krylov@gmail.com>
2022-12-10 08:38:58 +0300

    mui added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
-import React  from "react";
+import React from "react";
 import { PieChart, Pie, Cell, Legend } from "recharts";
+import Container from '@mui/material/Container';
 import values from './data.json';
 
 const COLORS = ["#FF6464", "#2A0944", "#A10035", "#3FA796"];
-const nameMap ={
-  notViewed:'Не просмотрено',
-  viewed:'Просмотрено',
-  rejected:'Отказ',
-  interview:'Приглашение'
+const nameMap = {
+  notViewed: 'Не просмотрено',
+  viewed: 'Просмотрено',
+  rejected: 'Отказ',
+  interview: 'Приглашение'
 
 }
 const valuesObj = {};
 values.data.forEach((v => {
-    if (valuesObj.hasOwnProperty(v.stat)) {
-        valuesObj[v.stat] = valuesObj[v.stat] + 1;
-    } else {
-        valuesObj[v.stat] = 1;
-    }
+  if (valuesObj.hasOwnProperty(v.stat)) {
+    valuesObj[v.stat] = valuesObj[v.stat] + 1;
+  } else {
+    valuesObj[v.stat] = 1;
+  }
 }))
-
 const data = []
 Object.keys(valuesObj).forEach((v) => {
-    data.push({ name: nameMap[v], value: valuesObj[v] })
+  data.push({ name: nameMap[v], value: valuesObj[v] })
 })
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({
@@ -37,7 +37,6 @@ const renderCustomizedLabel = ({
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
   return (
     <text
       x={x}
@@ -52,23 +51,25 @@ const renderCustomizedLabel = ({
 };
 export default function App() {
   return (
-    <PieChart width={400} height={400}>
-      <Legend />
-      <Pie
-        data={data}
-        cx={200}
-        cy={200}
-        labelLine={false}
-        label={renderCustomizedLabel}
-        outerRadius={80}
-        fill="#8884d8"
-        dataKey="value"
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-    </PieChart>
+    <Container maxWidth="sm">
+      <PieChart width={400} height={400}>
+        <Legend />
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </Container>
   );
 }
 
